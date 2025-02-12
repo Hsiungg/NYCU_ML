776134e909aa89ea0a2df0de7cf5bfe0166d59da
Hsiungg <hsiung4ever@gmail.com>
2025-02-12 17:54:35 +0800

    upload file

---
+++ LAB7/tsne.py
@@ -0,0 +1,243 @@
+#
+#  tsne.py
+#
+# Implementation of t-SNE in Python. The implementation was tested on Python
+# 2.7.10, and it requires a working installation of NumPy. The implementation
+# comes with an example on the MNIST dataset. In order to plot the
+# results of this example, a working installation of matplotlib is required.
+#
+# The example can be run by executing: `ipython tsne.py`
+#
+#
+#  Created by Laurens van der Maaten on 20-12-08.
+#  Copyright (c) 2008 Tilburg University. All rights reserved.
+
+import numpy as np
+import pylab
+import imageio
+import os
+
+
+def Hbeta(D=np.array([]), beta=1.0):
+    """
+        Compute the perplexity and the P-row for a specific value of the
+        precision of a Gaussian distribution.
+    """
+
+    # Compute P-row and corresponding perplexity
+    P = np.exp(-D.copy() * beta)
+    sumP = sum(P)
+    H = np.log(sumP) + beta * np.sum(D * P) / sumP
+    P = P / sumP
+    return H, P
+
+
+def x2p(X=np.array([]), tol=1e-5, perplexity=30.0):
+    """
+        Performs a binary search to get P-values in such a way that each
+        conditional Gaussian has the same perplexity.
+    """
+
+    # Initialize some variables
+    print("Computing pairwise distances...")
+    (n, d) = X.shape
+    sum_X = np.sum(np.square(X), 1)
+    D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)     #Dij = ||Xi||^2 - 2*Xi*Xj + ||Xj||^2
+    P = np.zeros((n, n))
+    beta = np.ones((n, 1))
+    logU = np.log(perplexity)
+
+    # Loop over all datapoints
+    for i in range(n):
+
+        # Print progress
+        if i % 500 == 0:
+            print("Computing P-values for point %d of %d..." % (i, n))
+
+        # Compute the Gaussian kernel and entropy for the current precision
+        betamin = -np.inf
+        betamax = np.inf
+        Di = D[i, np.concatenate((np.r_[0:i], np.r_[i+1:n]))]
+        (H, thisP) = Hbeta(Di, beta[i])
+
+        # Evaluate whether the perplexity is within tolerance
+        Hdiff = H - logU
+        tries = 0
+        while np.abs(Hdiff) > tol and tries < 50:
+
+            # If not, increase or decrease precision
+            if Hdiff > 0:
+                betamin = beta[i].copy()
+                if betamax == np.inf or betamax == -np.inf:
+                    beta[i] = beta[i] * 2.
+                else:
+                    beta[i] = (beta[i] + betamax) / 2.
+            else:
+                betamax = beta[i].copy()
+                if betamin == np.inf or betamin == -np.inf:
+                    beta[i] = beta[i] / 2.
+                else:
+                    beta[i] = (beta[i] + betamin) / 2.
+
+            # Recompute the values
+            (H, thisP) = Hbeta(Di, beta[i])
+            Hdiff = H - logU
+            tries += 1
+
+        # Set the final row of P
+        P[i, np.concatenate((np.r_[0:i], np.r_[i+1:n]))] = thisP
+    # Return final P-matrix
+    print("Mean value of sigma: %f" % np.mean(np.sqrt(1 / beta)))
+    return P
+
+
+def pca(X=np.array([]), no_dims=50):
+    """
+        Runs PCA on the NxD array X in order to reduce its dimensionality to
+        no_dims dimensions.
+    """
+
+    print("Preprocessing the data using PCA...")
+    (n, d) = X.shape
+    X = X - np.tile(np.mean(X, 0), (n, 1))
+    (l, M) = np.linalg.eig(np.dot(X.T, X))
+    Y = np.dot(X, M[:, 0:no_dims])
+    return Y
+
+
+def tsne(X=np.array([]), no_dims=2, initial_dims=50, perplexity=30.0):
+    """
+        Runs t-SNE on the dataset in the NxD array X to reduce its
+        dimensionality to no_dims dimensions. The syntaxis of the function is
+        `Y = tsne.tsne(X, no_dims, perplexity), where X is an NxD NumPy array.
+    """
+
+    # Check inputs
+    if isinstance(no_dims, float):
+        print("Error: array X should have type float.")
+        return -1
+    if round(no_dims) != no_dims:
+        print("Error: number of dimensions should be an integer.")
+        return -1
+
+    # Initialize variables
+    X = pca(X, initial_dims).real
+    (n, d) = X.shape
+    max_iter = 1000
+    initial_momentum = 0.5
+    final_momentum = 0.8
+    eta = 500
+    min_gain = 0.01
+    Y = np.random.randn(n, no_dims)
+    dY = np.zeros((n, no_dims))
+    iY = np.zeros((n, no_dims))
+    gains = np.ones((n, no_dims))
+    Ys = []
+    # Compute P-values
+    P = x2p(X, 1e-5, perplexity)
+    P = P + np.transpose(P)
+    P = P / np.sum(P)
+    P = P * 4.									# early exaggeration
+    P = np.maximum(P, 1e-12)
+
+    # Run iterations
+    for iter in range(max_iter):
+
+        # Compute pairwise affinities
+        sum_Y = np.sum(np.square(Y), 1)
+        num = -2. * np.dot(Y, Y.T)
+        num = 1. / (1. + np.add(np.add(num, sum_Y).T, sum_Y))
+        num[range(n), range(n)] = 0.
+        Q = num / np.sum(num)
+        Q = np.maximum(Q, 1e-12)
+
+        # Compute gradient
+        PQ = P - Q
+        for i in range(n):
+            dY[i, :] = np.sum(np.tile(PQ[:, i] * num[:, i], (no_dims, 1)).T * (Y[i, :] - Y), 0)
+
+        # Perform the update
+        if iter < 20:
+            momentum = initial_momentum
+        else:
+            momentum = final_momentum
+        gains = (gains + 0.2) * ((dY > 0.) != (iY > 0.)) + \
+                (gains * 0.8) * ((dY > 0.) == (iY > 0.))
+        gains[gains < min_gain] = min_gain
+        iY = momentum * iY - eta * (gains * dY)
+        Y = Y + iY
+        Y = Y - np.tile(np.mean(Y, 0), (n, 1))
+
+        # Compute current value of cost function
+        if (iter + 1) % 10 == 0:
+            C = np.sum(P * np.log(P / Q))
+            print("Iteration %d: error is %f" % (iter + 1, C))
+            Ys.append([Y[:, 0], Y[:, 1]])
+
+        # Stop lying about P-values
+        if iter == 100:
+            P = P / 4.
+
+    make_gif(Ys, perplexity)
+    plot_similarity(P, Q, perplexity)
+    # Return solution
+    return Y
+def make_gif(Y, perplexity):
+    gif_path = f'./tsne/tsne_{perplexity}.gif'
+    out_img = []
+    for i in range(len(Y)):
+        pylab.clf()
+        pylab.scatter(Y[i][0], Y[i][1], 10, labels)
+        pylab.grid(False)
+        pylab.axis('off')
+        pylab.draw()
+        fig = pylab.gcf()
+        fig.canvas.draw()
+        img= np.array(fig.canvas.renderer.buffer_rgba())
+        img= img[..., :3]
+        out_img.append(img)
+    imageio.mimsave(gif_path, out_img, duration=10)
+    pylab.close()
+
+def plot_similarity(P, Q, perplexity):
+    order = np.argsort(labels)
+    P = np.log(P)
+    Q = np.log(Q)
+    sorted_P = P[order, :][:, order]
+    sorted_Q = Q[order, :][:, order]
+    fig, (ax1, ax2) = pylab.subplots(1, 2, figsize=(20, 10))
+
+    cax1 = ax1.matshow(sorted_P, interpolation='nearest', cmap='viridis', vmin=np.min(P), vmax=np.max(P))
+    ax1.set_title('Reordered similarities in high-dimensional space')
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+    ax1.set_xticklabels([])
+    ax1.set_yticklabels([])
+    fig.colorbar(cax1, ax=ax1)
+
+    cax2 = ax2.matshow(sorted_Q, interpolation='nearest', cmap='viridis', vmin=np.min(Q), vmax=np.max(Q))
+    ax2.set_title('Reordered similarities in low-dimensional space')
+    ax2.set_xticks([])
+    ax2.set_yticks([])
+    ax2.set_xticklabels([])
+    ax2.set_yticklabels([])
+    fig.colorbar(cax2, ax=ax2)
+    pylab.tight_layout()
+    pylab.savefig(f'./tsne/similarities_tsne_{perplexity}.png')
+    pylab.close()
+
+if __name__ == "__main__":
+    print("Run Y = tsne.tsne(X, no_dims, perplexity) to perform t-SNE on your dataset.")
+    print("Running example on 2,500 MNIST digits...")
+    os.makedirs('./tsne', exist_ok=True)
+    X = np.loadtxt("mnist2500_X.txt")
+    labels = np.loadtxt("mnist2500_labels.txt")
+    perplexities = [5.0, 10.0, 20.0, 50.0, 100.0]
+    #perplexity = 20.0
+    for perplexity in perplexities:
+        Y = tsne(X, 2, 50, perplexity)
+        pylab.scatter(Y[:, 0], Y[:, 1], 10, labels)
+        pylab.grid(False)
+        pylab.axis('off')
+        pylab.savefig(f'./tsne/final_tsne_p={perplexity}.png')
+        pylab.close()
